8b5e7f1cec2850f9bdf377bed25efd30c1699665
PrinceB3N <39066183+PrinceB3N@users.noreply.github.com>
2018-05-11 17:20:43 -0700

    Added Mouselistener and concurrent image download
    -Mouselistener now listens for clicks and terminates
    -Moved IOImage reading to wait time in-between image transitions to prevent massive lag at beginning

--- IdleImagesScreen-master/RunIdleScreen.java
+++ IdleImagesScreen-master/RunIdleScreen.java
@@ -7,54 +7,61 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class RunIdleScreen implements MouseListener{
-	private static boolean isRunning = true;
+public class runner extends JFrame implements MouseListener{
+	private boolean isRunning = true;
 	
-	public static void startidlescreen() throws IOException{
+	public void startidlescreen() throws IOException{
 		isRunning =true;
         //Initialize storage and access of images to be used in screensaver
-		String folderpath = "Images";
+		String folderpath = "images";
 	 File dir = new File(folderpath);
 	    File[] directoryListing = dir.listFiles();
 	    ArrayList<BufferedImage> pics = new ArrayList<BufferedImage>(0);
-	    
-	    //Move jpg files into BufferedImages
-	    for(File fi: directoryListing) {
 	    	
-	    	pics.add(ImageIO.read(fi));
-	    	
-	    	
-	    }
-	  //Endless iteration through the images until screen is clicked
+	  //Endless iteration through the images
 	   
-	    	for(int i=0;i<pics.size();i=(i+1)%pics.size()) {
+	    	for(int i=0;i<directoryListing.length;i=(i+1)%directoryListing.length) {
 	    		
-	    	System.out.println("start");
-	    	JLabel label = new JLabel(new ImageIcon(pics.get(i)));
+	    	System.out.println(pics.size());
+	    	if(pics.size()==0) {
+	    		pics.add(ImageIO.read(directoryListing[0]));
+	    	}
+	    	
+	    	 JLabel label = new JLabel(new ImageIcon(pics.get(i)));
+	    	
 	    	JFrame f = new JFrame();
 	    	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    	f.getContentPane().add(label);
-	    	f.getContentPane().addMouseListener(null);
+	    	f.getContentPane().addMouseListener(this);
 	    	f.pack();
 	    	f.setLocation(0,0);
 	    	f.setVisible(true);
 	    	
-	    	//Part that breaks out of method when click
-	    	if(!isRunning) {
-	    		break;
-	    	}
-		
-		//Pause before changing photos
-	    	GameTimer g = new GameTimer(2);
+		//Can change time in here to change transition times
+	    	GameTimer g = new GameTimer(5);
+	    	boolean donedownloading= false;
 	    	while(true) {
 	    		System.out.print("");
-	    		if(g.getTimeRemaining()<=0) {
+	    		
+	    		//Concurrent image loading
+	    		if(pics.size() < directoryListing.length && !donedownloading) {
+		    		pics.add(ImageIO.read(directoryListing[(i+1)%directoryListing.length]));
+		    		donedownloading=true;
+	    		}
+			//Terminate condition after user taps screen
+	    		if(!isRunning) {
+	    			f.dispose();
+		    		return;
+	    		}
+			//Exit condition for loop based on timer time fulfilled
+		    	else if(g.getTimeRemaining()<=0) {
 	    			break;
 	    		}
+	    		
 	    	}
-		
-		//clears the original jframe to avoid stacking
+	    	//Gets rid of old jframe
 	    	f.dispose();
+	    	
 	    	}
 	    
 }
@@ -62,19 +69,20 @@ public class RunIdleScreen implements MouseListener{
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		isRunning=false;
-		
+		System.out.print("m");
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		isRunning=false;
+		
+		
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		isRunning=false;
+		
 	}
 
 	@Override
@@ -86,6 +94,15 @@ public class RunIdleScreen implements MouseListener{
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		isRunning=false;
+		
+	}
+	public static void main(String[] args) {
+		runner r = new runner();
+		try {
+			r.startidlescreen();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
